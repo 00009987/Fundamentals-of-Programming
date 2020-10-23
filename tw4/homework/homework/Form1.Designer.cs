310018246990cd98a50e127959de02d6f401b14e
00009987 <72728843+00009987@users.noreply.github.com>
2020-10-23 16:56:47 +0500

    uploading the codes of homework from tw4

---
+++ tw4/homework/homework/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userInput
+            // 
+            this.userInput.Location = new System.Drawing.Point(61, 59);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(100, 20);
+            this.userInput.TabIndex = 0;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(61, 97);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(100, 23);
+            this.submitBtn.TabIndex = 1;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 176);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.userInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Button submitBtn;
+    }
+}
+
---
+++ tw4/homework/homework/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            //receiving the user input as an integer
+            var input = Convert.ToInt32(userInput.Text);
+            //declaring my guess as an answer to the input's square root
+            var guess = 1.0;
+
+            
+            for(double i = 1; i <= input / 2; i += 0.5)
+            {
+                //checking whether difference between the actual input and guess's square is less than 0.01,
+                // if so the guess is almost the same as the actual answer
+                if (Math.Abs(input - guess*guess) < 0.01)
+                {
+                    break;
+                }
+                //increasing or improving the guess to a better version
+                else
+                {
+                    guess = (i + input / i)/2;
+                }
+            }
+
+            //showing the user first actual answer and the application's guess
+            MessageBox.Show(String.Format("Actual answer is {0} \nGuess is {1}", Math.Sqrt(input), guess));
+        }
+    }
+}

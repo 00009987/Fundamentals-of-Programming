fa3b1f672c98400487a90fa5d62c91ff1155a47c
00009987 <72728843+00009987@users.noreply.github.com>
2020-11-10 01:35:17 +0500

    Add files via upload

---
+++ tw5/MergeArrays/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MergeArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbxInputOne = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.tbxInputTwo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxInputOne
+            // 
+            this.tbxInputOne.Location = new System.Drawing.Point(35, 60);
+            this.tbxInputOne.Name = "tbxInputOne";
+            this.tbxInputOne.Size = new System.Drawing.Size(209, 20);
+            this.tbxInputOne.TabIndex = 0;
+            // 
+            // tbxInputTwo
+            // 
+            this.tbxInputTwo.Location = new System.Drawing.Point(35, 112);
+            this.tbxInputTwo.Name = "tbxInputTwo";
+            this.tbxInputTwo.Size = new System.Drawing.Size(209, 20);
+            this.tbxInputTwo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Second input";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(35, 150);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(106, 29);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(35, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 38);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Please enter your inputs using comma. For example name, surname, number...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 205);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxInputTwo);
+            this.Controls.Add(this.tbxInputOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInputOne;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox tbxInputTwo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ tw5/MergeArrays/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MergeArrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //getting data from the user
+            var A = tbxInputOne.Text.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var B = tbxInputTwo.Text.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            //creating an array with the size of equal to previous two
+            var C = new string[A.Length + B.Length];
+            var index = 0; //to keep track of indexs in array C
+
+            //looping through array A
+            foreach (var a in A)
+            {
+                //checking if array C already contains an element from array A
+                if (!C.Contains(a))
+                {
+                    //if not adding it to array C
+                    C[index] = a;
+                    // and incrementing index value so that next element will be added in its order
+                    index++;
+                }
+            }
+
+            foreach (var b in B)
+            {
+                //checking if array C already contains an element from array B
+                if (!C.Contains(b))
+                {
+                    //if not adding it to array C
+                    C[index] = b;
+                    // and incrementing index value so that next element will be added in its order
+                    index++;
+                }
+            }
+
+            //refactoring and resizing array C to get rid of not used empty placeholders
+            Array.Resize(ref C, index);
+            Array.Sort(C);
+
+            var message = String.Join(", ", C);
+
+            MessageBox.Show(message);
+        }
+    }
+}

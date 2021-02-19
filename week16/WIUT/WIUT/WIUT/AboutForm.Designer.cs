7579befb0eb31943a9c91305d15c84db8b3aa515
00009987 <72728843+00009987@users.noreply.github.com>
2021-02-19 10:30:07 +0500

    Add files via upload

---
+++ week16/WIUT/WIUT/WIUT/AboutForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WIUT
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAbove = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAbove
+            // 
+            this.lblAbove.AutoSize = true;
+            this.lblAbove.Location = new System.Drawing.Point(20, 20);
+            this.lblAbove.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAbove.Name = "lblAbove";
+            this.lblAbove.Size = new System.Drawing.Size(385, 20);
+            this.lblAbove.TabIndex = 0;
+            this.lblAbove.Text = "WIUT Admission System Application done for FunPro";
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 89);
+            this.Controls.Add(this.lblAbove);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WIUT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAbove;
+    }
+}
